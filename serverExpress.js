898c8899ebe8ecddeb3f4e4b25837bd60e788d22
Abir <abir.laraich@etu.univ-lehavre.fr>
2024-06-03 19:48:53 +0200

    cors policy

--- serverExpress.js
+++ serverExpress.js
@@ -11,11 +11,12 @@ const app = express()
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
-  });
+});
+
 app.use(express.json());
 
 
